a3c323d83aa6fc7d1548a53674afe62a48e70c4b
Neha.Singh <neha.singh1@1MG-LPTP-970.local>
2020-08-05 00:30:24 +0530

    first day code added

---
+++ august/detectCapital.js
@@ -0,0 +1,18 @@
+// Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+// We define the usage of capitals in a word to be right when one of the following cases holds:
+
+// All letters in this word are capitals, like "USA".
+// All letters in this word are not capitals, like "leetcode".
+// Only the first letter in this word is capital, like "Google".
+// Otherwise, we define that this word doesn't use capitals in a right way.
+
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+
+var detectCapitalUse = function(word) {
+    return word === word.toUpperCase() || word === word.toLowerCase() || word === word[0].toUpperCase() + word.slice(1).toLowerCase() 
+};
