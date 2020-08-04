afc1a5ec4a8a575d637a69e967451593f0a71757
Neha.Singh <nehachauhan79@gmail.com>
2020-08-05 00:48:58 +0530

    Testing commit

--- august/detectCapital.js
+++ august/detectCapital.js
@@ -16,3 +16,5 @@
 var detectCapitalUse = function(word) {
     return word === word.toUpperCase() || word === word.toLowerCase() || word === word[0].toUpperCase() + word.slice(1).toLowerCase() 
 };
+
+// Testing
